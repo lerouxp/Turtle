ac34d802088157f0b4e71b531f4f5067b714cfca
lerouxp <ph.leroux@numericable.fr>
2015-09-08 13:00:52 +0200

    start

---
+++ app/src/main/java/com/epitech/poufpouf/turtle/MapsActivity.java
@@ -0,0 +1,47 @@
+package com.epitech.poufpouf.turtle;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_maps);
+        //setUpMapIfNeeded();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trajet);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //setUpMapIfNeeded(); //pour pas googol
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+}
